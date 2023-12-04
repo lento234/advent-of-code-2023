2f7b1f8ad65555adad8c3e9a9eabe8a365023d6b
Lento Manickathan <lento.manickathan@gmail.com>
2023-12-04 08:17:56 +0100

    wip

--- day01/day01.zig
+++ day01/day01.zig
@@ -1,33 +1,75 @@
 const std = @import("std");
 
+// aliases
+const expect = std.testing.expect;
+const print = std.debug.print;
+
+// allocator
+var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+// defer arena.deinit();
+const alloc = arena.allocator();
+
 fn isNumber(char: u8) bool {
-    return switch (char) {
-        '0'...'9' => true,
-        else => false,
-    };
+    return (char >= '0') and (char <= '9');
 }
 
 pub fn main() !void {
-    // std.debug.print("Advent of code: day {d}\n", .{1});
-    std.log.info("Advent of code: day {}", .{1});
+    print("Advent of code: day {}\n", .{1});
 
-    // Read file
+    // Parse input
     var buffer: [32768]u8 = undefined;
-    // const data = try std.fs.cwd().readFile("test_input.txt", &buffer);
     const data = try std.fs.cwd().readFile("input.txt", &buffer);
-    const len = data.len;
-    std.log.info("{} bytes read!", .{len});
+
+    // Part 1
+    var result = try part1(data);
+    print("Part 1: {}\n", .{result});
+}
+
+test "part 1" {
+    const input =
+        \\ 1abc2
+        \\ pqr3stu8vwx
+        \\ a1b2c3d4e5f
+        \\ treb7uchet
+    ;
+    try expect(@as(u32, 142) == part1(input) catch 0);
+}
+
+test "part 2" {
+    const input =
+        \\ two1nine
+        \\ eightwothree
+        \\ abcone2threexyz
+        \\ xtwone3four
+        \\ 4nineeightseven2
+        \\ zoneight234
+        \\ 7pqrstsixteen
+    ;
+    try expect(@as(u32, 281) == part2(input) catch 0);
+}
+
+fn part2(data: []const u8) !u32 {
 
     // Split data to lines
     var lines = std.mem.tokenize(u8, data, "\n");
 
-    // allocator
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const alloc = arena.allocator();
+    var i: u8 = 0;
+    while (lines.next()) |line| {
+        if (std.mem.indexOf(u8, "one", 0) != null) {
+            print("{}: {s}\n", .{ i, line });
+        }
+        i += 1;
+    }
+
+    return 0;
+}
+
+fn part1(data: []const u8) !u32 {
+
+    // Split data to lines
+    var lines = std.mem.tokenize(u8, data, "\n");
 
     // Inspect text
-    // var i: u32 = 0;
     var result: u32 = 0;
     while (lines.next()) |line| {
         var list = std.ArrayList(u8).init(alloc);
@@ -44,5 +86,5 @@ pub fn main() !void {
         result += @as(u32, value);
     }
 
-    std.log.info("Result: {}", .{result});
+    return result;
 }
