5f090ebb29a1b5096f0c3e9623e6d9e6944347c3
lento234 <lento.manickathan@gmail.com>
2023-12-09 15:47:36 +0100

    solved

--- day09/day09.go
+++ day09/day09.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func history(numbers []int) int {
+func history(numbers []int, index int, operator func(a, b int) int) int {
 
 	// check if layer is all zeros
 	allZeros := true
@@ -21,7 +21,9 @@ func history(numbers []int) int {
 		next = append(next, numbers[i]-numbers[i-1])
 	}
 
-	return numbers[len(numbers)-1] + history(next)
+	idx := (len(numbers) + index) % len(numbers)
+
+	return operator(numbers[idx], history(next, index, operator))
 }
 
 func part1(input []string) int {
@@ -31,18 +33,22 @@ func part1(input []string) int {
 		// Parse numbers
 		numbers := utils.StringToNumbers(line, " ")
 		// Recursive history
-		result += history(numbers)
+		result += history(numbers, -1, func(a, b int) int { return a + b })
 	}
-
-	// calc history
-	// 1992273652
 	return result
 }
 
-// func part2(input []string) int {
-// 	result := 0
-// 	return result
-// }
+func part2(input []string) int {
+	result := 0
+
+	for _, line := range input {
+		// Parse numbers
+		numbers := utils.StringToNumbers(line, " ")
+		// Recursive history
+		result += history(numbers, 0, func(a, b int) int { return a - b })
+	}
+	return result
+}
 
 func Solve() {
 	// Parse input
@@ -52,7 +58,7 @@ func Solve() {
 	result := part1(input)
 	fmt.Printf("%s: %v\n", utils.FormatGreen("Part 1"), result)
 
-	// // Part 2
-	// result = part2(input)
-	// fmt.Printf("%s: %v\n", utils.FormatGreen("Part 2"), result)
+	// Part 2
+	result = part2(input)
+	fmt.Printf("%s: %v\n", utils.FormatGreen("Part 2"), result)
 }
--- day09/day09_test.go
+++ day09/day09_test.go
@@ -23,17 +23,19 @@ func TestPart1(t *testing.T) {
 	fmt.Printf("Part 1 [%s]: %v == %v\n", utils.FormatGreen("solved"), answer, result)
 }
 
-// func TestPart2(t *testing.T) {
+func TestPart2(t *testing.T) {
 
-// 	// Test part 2
-// 	puzzle := `<puzzle>`
-// 	answer := `<answer>`
+	// Test part 2
+	puzzle := `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`
+	answer := 2
 
-// 	// Solve
-// 	result := part2(utils.ParseString(puzzle))
+	// Solve
+	result := part2(utils.ParseString(puzzle))
 
-// 	if result != answer {
-// 		t.Fatalf("Failed!: %v != %v", answer, result)
-// 	}
-// 	fmt.Printf("Part 2 [%s]: %v == %v\n", utils.FormatGreen("solved"), answer, result)
-// }
+	if result != answer {
+		t.Fatalf("Failed!: %v != %v", answer, result)
+	}
+	fmt.Printf("Part 2 [%s]: %v == %v\n", utils.FormatGreen("solved"), answer, result)
+}
